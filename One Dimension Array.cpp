b0fd9844034b00398020295293532d6c70490d80
diksha-g18 <gujarprakash81@gmail.com>
2026-03-27 13:54:50 +0530

    Add files via upload

---
+++ One Dimension Array.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int array[2]={};
+	int i;
+	
+	printf("Elements of the array are: \n");
+	for(i = 0; i < 10; i++){
+		printf("array[%d] = %d\n", i, array[i]);
+			}
+			return 0;
+}
